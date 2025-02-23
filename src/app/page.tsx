e0cb4c9e641d5374ed26439eebc21aabe1294155
Marvin Ernesto <marvin.lagas01234@gmail.com>
2025-02-23 12:57:14 +0200

    Setup formatter (Biome Js)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,27 @@
+import { Button } from "@/components/button";
+import { IconButton } from "@/components/icon-button";
+import { InputField, InputIcon, InputRoot } from "@/components/input";
+import { ArrowRight, Copy, Mail } from "lucide-react";
+
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <main>
+      <Button type="submit">
+        Enviar
+        <ArrowRight />
+      </Button>
+
+      <IconButton>
+        <Copy />
+      </IconButton>
+      <div>
+        <InputRoot>
+          <InputIcon>
+            <Mail  className="size-5"/>
+          </InputIcon>
+          <InputField />
+        </InputRoot>
+      </div>
+    </main>
+  );
 }
